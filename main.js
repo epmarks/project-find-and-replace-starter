d313621190497a0f5ba53eb1daf23b7c4abf5495
Emery <epmarks@ymail.com>
2020-10-30 17:52:01 -0400

    code partially working

--- main.js
+++ main.js
@@ -1,38 +1,45 @@
 // You should NOT change the HTML or CSS in this project (at least until you reach
 // the bonus objectives). Focus on the JavaScript.
 
-const findInput = document.querySelector(".find-input")
-const replaceInput = document.querySelector(".replace-input")
-const replaceAllButton = document.querySelector(".replace-all-button")
+const findInput = document.querySelector(".find-input");
+const replaceInput = document.querySelector(".replace-input");
+const replaceAllButton = document.querySelector(".replace-all-button");
 
 // The following variable holds your OUTER ARRAY of row elements.
 // Later you will need an OUTER LOOP to loop over the individual elements within
 // this array.
-const rowElements = document.querySelectorAll(".row")
+const rowElements = document.querySelectorAll(".row");
 
-// When you call the function belwo, it will get and return an INNER ARRAY
+// for (i = 0; i < rowElements.length; i += 1) {
+//     const currentRowElement = rowElements[i];
+
+//     currentRowElement.addEventListener("click", function(event) {
+//         const clickedRowElement = event.target;
+
+//         if () {}
+//     }
+// });
+
+// When you call the function below, it will get and return an INNER ARRAY
 // containing the cell elements for a given row.
 // Call this function from WITHIN your row elements loop. Then you will, in turn,
 // need to loop over the resulting cell elements. But where should this whole
-// NESTED LOOP go? Think through the user's experience: when should WHAT happen? 
-function getCellElements (currentRowElement) {
-    return currentRowElement.querySelectorAll(".cell")
+// NESTED LOOP go? Think through the user's experience: when should WHAT happen?
+function getCellElements(currentRowElement) {
+  return currentRowElement.querySelectorAll(".cell");
 }
 
+replaceAllButton.addEventListener("click", function () {
+  let userInput = findInput.Value;
+  let replacement = replaceInput.Value;
 
-// YOUR CODE GOES HERE
-
-
-// One last thing: dedicate very careful attention to using variables and
-// naming them accurately.
-// And when you change the value you are assigning to a variable, don't
-// forget to consider changing the name to reflect the change you made! It
-// is very easy to get confused when you are working inside NESTED LOOPS.
-// The best of us do. And unnecessary confusion during the process of 
-// developing your code means wasted time.
-//
-// The time-cost of structuring and naming things well is FAR less than the
-// time-cost of ignoring the quality and readability of your code.
-//
-// You can, of course, remove any comments in this starter project once
-// you have read them, if you prefer.
+  for (i = 0; i < rowElements.length; i++) {
+    const currentRowElement = rowElements[i];
+    getCellElements(currentRowElement);
+    console.log(currentRowElement);
+    for (j = 0; j < currentRowElement.length; j++) {
+      const currentCell = currentRowElement[j];
+      console.log(currentCell);
+    }
+  }
+});
